05c77443d7f0f177786d80e41bcc4904db1c1335
Sneyder <sneyder2328@gmail.com>
2019-12-16 16:40:24 -0400

    Test heroku connection

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,9 +5,16 @@ const router = require('../src/routes/index');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.get('/test', (req, res) => {
+    const {config} = require('../src/config/config');
+    console.log("Hello tetst", config.orm.dialect);
+    res.send("Hello tetst " + config.orm.dialect);
+});
+
 app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', router);
+
 
 app.listen(port, () => {
     console.log(`Started on port  ${port}`);
