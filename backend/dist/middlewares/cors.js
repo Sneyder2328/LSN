9582e3aa7f177db7fce82f13c45f4982a9df44b0
Sneyder <sneyder2328@gmail.com>
2020-01-14 14:20:21 -0400

    Fix bug in last commit

--- backend/dist/middlewares/cors.js
+++ backend/dist/middlewares/cors.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.corsOptions = {
     origin: process.env.NODE_ENV === 'production'
-        ? [/mystifying-liskov-d8ef00.netlify.com/]
+        ? [/mystifying-liskov-d8ef00.netlify.com/, /localhost/]
         : [/localhost/],
     credentials: true,
     allowedHeaders: 'Content-Type, authorization, authorization-refresh-token, X-Requested-With, Accept',
