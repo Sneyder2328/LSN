ad1907b70e34837978b28bfc4228cdda1c9f2d5f
Sneyder Angulo <sneyder@mercury.cash>
2022-05-23 09:22:21 -0400

    fix: update background

--- frontend/src/pages/Auth/index.tsx
+++ frontend/src/pages/Auth/index.tsx
@@ -1,32 +1,34 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import "./styles.scss"
 import classnames from 'classnames';
-import {useSelector} from "react-redux";
-import {Redirect} from "react-router-dom";
-import {AppState} from "../../modules/rootReducer";
-import {LogIn} from "./LogIn";
-import {SignUp} from "./SignUp";
+import { useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { AppState } from "../../modules/rootReducer";
+import { LogIn } from "./LogIn";
+import { SignUp } from "./SignUp";
 
 export const AuthForm = () => {
-    const isLoggedIn = useSelector((state: AppState) => state.auth.isAuthenticated)
-    const [isLoginSelected, setLoginSelected] = useState<boolean>(false);
+  const isLoggedIn = useSelector((state: AppState) => state.auth.isAuthenticated)
+  const [isLoginSelected, setLoginSelected] = useState<boolean>(false);
 
-    if (isLoggedIn)
-        return (<Redirect to={{pathname: '/'}}/>);
+  if (isLoggedIn)
+    return (<Redirect to={{ pathname: '/' }} />);
 
-    const Content = isLoginSelected ? <LogIn/> : <SignUp/>;
-    return (
-        <div className='auth-form'>
-            <div id="background"/>
-            <div className='buttons-container'>
-                <button className={classnames({'selected': !isLoginSelected})} onClick={() => setLoginSelected(false)}>
-                    Sign up
-                </button>
-                <button className={classnames({'selected': isLoginSelected})} onClick={() => setLoginSelected(true)}>
-                    Log In
-                </button>
-            </div>
-            {Content}
+  const Content = isLoginSelected ? <LogIn /> : <SignUp />;
+  return (
+    <>
+      <div id="background" />
+      <div className='auth-form'>
+        <div className='buttons-container'>
+          <button className={classnames({ 'selected': !isLoginSelected })} onClick={() => setLoginSelected(false)}>
+            Sign up
+          </button>
+          <button className={classnames({ 'selected': isLoginSelected })} onClick={() => setLoginSelected(true)}>
+            Log In
+          </button>
         </div>
-    );
+        {Content}
+      </div>
+    </>
+  );
 };
